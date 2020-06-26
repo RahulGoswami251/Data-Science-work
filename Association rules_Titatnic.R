4f9b9d76090b6b8c13d9c40166fe09e2b9b8194e
Rahul Goswami <65107372+RahulGoswami251@users.noreply.github.com>
2020-06-26 20:50:03 +0530

    Add files via upload

---
+++ Association rules_Titatnic.R
@@ -0,0 +1,18 @@
+
+
+#Association Rules
+install.packages("arules")
+library(arules)
+Titanic<-read.csv("/Volumes/Data/Course Content/DS content/A.Rules/Titanic.csv")
+
+Titanic<-Titanic[,-c(1)]
+rules <- apriori(Titanic)
+arules::inspect(rules)
+rules.sorted <- sort(rules, by="lift")
+arules::inspect(rules.sorted)
+
+# rules with rhs containing "Survived" only
+rules <- apriori(Titanic,parameter = list(supp=0.1, conf=0.5)
+                 ,appearance = list(rhs=c("Survived=No", "Survived=Yes")
+                 ),control = list(verbose=F))
+arules::inspect(rules)
